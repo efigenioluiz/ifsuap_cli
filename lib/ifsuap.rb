dd0bd05150c9c95c11659e9b429d8988e288e0ea
efigenioluiz <hanzou0012@gmail.com>
2024-11-14 00:58:50 -0300

    feat: documenting of the README to set cf

--- lib/ifsuap.rb
+++ lib/ifsuap.rb
@@ -13,8 +13,8 @@ module IfSuap
     def initialize(*args)
       super
       options = Selenium::WebDriver::Chrome::Options.new
-      # options.add_argument('--headless')
-      # options.add_argument('--no-sandbox')
+      options.add_argument('--headless')
+      options.add_argument('--no-sandbox')
       options.add_argument('--disable-dev-shm-usage')
       options.add_argument('--window-size=1280,800')
       options.add_argument('--disable-gpu')
